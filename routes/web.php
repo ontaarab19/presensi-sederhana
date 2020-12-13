7cd8af593ab1c704d83995cf11b1f1be74253a19
ontaarab19 <penaadit@gmail.com>
2020-12-13 12:30:55 +0800

    Presensi Keluar

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,7 @@ Route::group(['middleware' => ['auth', 'ceklevel:admin,karyawan']], function() {
 
 Route::group(['middleware' => ['auth', 'ceklevel:karyawan']], function() {
     route::post('/simpan-masuk',[PresensiController::class,'store'])->name('simpan-masuk');
+    route::post('ubah-presensi',[PresensiController::class,'presensipulang'])->name('ubah-presensi');
     route::get('/presensi-masuk',[PresensiController::class,'index'])->name('presensi-masuk');
+    route::get('/presensi-keluar',[PresensiController::class,'keluar'])->name('presensi-keluar');
 });
